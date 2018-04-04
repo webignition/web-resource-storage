cbe0706bee2ea92a78996da92952e551afc35250
Jon Cram <jon@webignition.net>
2018-04-04 13:02:19 +0100

    Import

---
+++ tests/ContentTypeFileExtensionMapTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace webignition\Tests\WebResource;
+
+use Mockery\Mock;
+use webignition\InternetMediaTypeInterface\InternetMediaTypeInterface;
+use webignition\WebResource\ContentTypeFileExtensionMap;
+
+class ContentTypeFileExtensionMapTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getFileExtensionDataProvider
+     *
+     * @param InternetMediaTypeInterface $internetMediaType
+     * @param string $expectedFileExtension
+     */
+    public function testGetFileExtension(InternetMediaTypeInterface $internetMediaType, $expectedFileExtension)
+    {
+        $this->assertEquals($expectedFileExtension, ContentTypeFileExtensionMap::getFileExtension($internetMediaType));
+    }
+
+    /**
+     * @return array
+     */
+    public function getFileExtensionDataProvider()
+    {
+        return [
+            'text/html' => [
+                'internetMediaType' => $this->createInternetMediaType('html'),
+                'expectedFileExtension' => 'html',
+            ],
+            'text/css' => [
+                'internetMediaType' => $this->createInternetMediaType('css'),
+                'expectedFileExtension' => 'css',
+            ],
+            'application/javascript' => [
+                'internetMediaType' => $this->createInternetMediaType('javascript'),
+                'expectedFileExtension' => 'js',
+            ],
+            'application/x-javascript' => [
+                'internetMediaType' => $this->createInternetMediaType('x-javascript'),
+                'expectedFileExtension' => 'js',
+            ],
+        ];
+    }
+
+    /**
+     * @param string $subtype
+     *
+     * @return Mock|InternetMediaTypeInterface
+     */
+    private function createInternetMediaType($subtype)
+    {
+        /* @var InternetMediaTypeInterface|Mock $internetMediaType */
+        $internetMediaType = \Mockery::mock(InternetMediaTypeInterface::class);
+
+        $internetMediaType
+            ->shouldReceive('getSubtype')
+            ->andReturn($subtype);
+
+        return $internetMediaType;
+    }
+}
